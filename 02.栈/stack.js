182d3f4fac4a4b38cc90b2afb76c1653b629c20a
zhangbin3 <zhangbin3>
2021-04-27 21:09:34 +0800

    mulu

--- 02.栈/stack.js
+++ 02.栈/stack.js
@@ -118,4 +118,47 @@ function print() {
 
 
 // 学习使用栈的最著名的三个算法示例：10进制转2进制，任意进制转换，平衡圆括号
+// 233 == 11101001
+// 2x(10x10) + 3x(10) + 3x(1)
+// function divideBy2(decNumber){
+//   var remStack = new Stack(),
+//       rem,
+//       binaryString = '';
+//   while (decNumber > 0){
+//       rem = Math.floor(decNumber % 2);
+//       remStack.push(rem);
+//       decNumber = Math.floor(decNumber / 2);
+//   }
+//   while (!remStack.isEmpty()){
+//       binaryString += remStack.pop().toString();
+//   }
+//   return binaryString;
+// }
+
+// console.log(divideBy2(233));
+// console.log(divideBy2(10));
+// console.log(divideBy2(1000));
+
+
+
+// 10进制转其他任意进制
+// function baseConverter(decNumber, base){
+//   var remStack = new Stack(),
+//       rem,
+//       baseString = '',
+//       digits = '0123456789ABCDEF';
+//   while (decNumber > 0){
+//       rem = Math.floor(decNumber % base);
+//       remStack.push(rem);
+//       decNumber = Math.floor(decNumber / base);
+//   }
+//   while (!remStack.isEmpty()){
+//       baseString += digits[remStack.pop()];
+//   }
+//   return baseString;
+// }
+
+// console.log(baseConverter(100345, 2));
+// console.log(baseConverter(100345, 8));
+// console.log(baseConverter(100345, 16));
 
